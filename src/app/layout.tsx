631740dbe4232d7353df5e075cb070284d9bc65a
ayame1111 <denismagryta@gmail.com>
2026-03-06 00:44:50 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default async function RootLayout({
         <Script 
           src="https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js" 
           strategy="beforeInteractive"
+          id="live2d-cubism-core"
         />
         <Script
             src="https://cdn.jsdelivr.net/gh/dylanNew/live2d/webgl/Live2D/lib/live2d.min.js"
